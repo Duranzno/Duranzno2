56944e15fd1cd342342bfd67e33341f2221a1624
Alejandro D <aledurax@gmail.com>
2020-10-01 13:23:59 -0400

    🎨 Enabled Cloudinary

--- src/cms/index.js
+++ src/cms/index.js
@@ -1,7 +1,10 @@
 // @flow strict
 import CMS from 'netlify-cms-app';
+import cloudinary from 'netlify-cms-media-library-cloudinary';
+
 import PagePreview from './preview-templates/page-preview';
 import PostPreview from './preview-templates/post-preview';
 
 CMS.registerPreviewTemplate('pages', PagePreview);
 CMS.registerPreviewTemplate('posts', PostPreview);
+CMS.registerMediaLibrary(cloudinary);
